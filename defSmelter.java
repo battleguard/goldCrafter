9a0b886984886aecf41a5a394f0fea9a17e32473
battleguard <howard.175@wright.edu>
2011-09-29 03:17:23 -0300

    Edited defSmelter.java via GitHub

--- defSmelter.java
+++ defSmelter.java
@@ -1,25 +1,20 @@
 // C:\ceg453\tone.c
-// This program produces a 4 KHz square wave and prints
-// a "Hello World" message to the screen every second.
-// It also flashes LED1 at a frequency of 0.5 Hz (i.e.,
-// on for one second and off for one second).
-// Assume that the E clock frequency is 24 MHz.
+//This program produces a C note or 261.6Hz frequency
 #include <mc9s12c32.h>
-void main() {
-	unsigned int count = 0;
-	TIOS |= 0x01; // use channel 0 for output compare
-	TSCR1 = 0x90; // enable timer and allow fast flag clear
-	TCTL2 = 0x01; // select toggle as the action for output compare
-	TC0 = TCNT + 3000; // set up TC0 and clear C0F flag
-	DDRA = 0xff; // set port A as output pins (for LED1)
-	while (1) {
-		while (!(TFLG1 & 0x01))
-			; // wait until C0F flag is set
-		TC0 = TCNT + 3000; // set up TC0 and clear C0F flag
-		if (count++ == 8000) {
-			puts("Hello World\r"); // same as printf("Hello World\n\r");
-			PORTA ^= 0x01; // toggle PA0 (for LED1)
-			count = 0;
-		}
-	}
+void main()
+{
+  unsigned int count = 0;
+ 
+
+  TIOS|=0x01; // use channel 0 for output compare
+  TSCR1=0x90; // enable timer and allow fast flag clear
+  TCTL2=0x01; // select toggle as the action for output compare
+  TC0=TCNT+45872; // set up TC0 and clear C0F flag
+
+ 
+  while(1)
+  {   
+    while(!(TFLG1&0x01)); // Wait till Flag ()TCO == TCNT)
+    TC0=TCNT+45872; // Update TCO to not set off flag until 45872 clocks & clear C0F   
+  }
 }
