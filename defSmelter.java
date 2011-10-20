df6d4f699bb15a91127b3b03fe59b798eaf863e8
battleguard <howard.175@wright.edu>
2011-10-20 14:27:01 -0300

    Week3 Prelab done

--- defSmelter.java
+++ defSmelter.java
@@ -1,27 +1,27 @@
-// C:\ceg453\tone.c
-//This program produces a C note or 261.6Hz frequency 
-// for a 1/8 measure (560ms) and pauses for 80ms between notes
 #include <mc9s12c32.h>
+
+unsigned int COUNT_VAL, DUTY_CYCLE=10;
+
+#pragma interrupt_handler tc0_isr
+void tc0_isr()
+{
+     COUNT_VAL = (PORTT & 0x01) ? 240 * DUTY_CYCLE : 12000;
+     TC0=TCNT + (PortA & 0x01) ? 240000- COUNT_VAL : COUNT_VAL;
+}
+
 void main()
 {
-  unsigned int count = 0;
- 
+     TSCR1 = 0x09; //fast flag clear
+     TIOS|=0x01; // use channel 0 for output compare
+     DDRT &= ~0x01;//port T as input
+     TC0=TCNT+12000;
+     TCTL2=0x01; // select toggle as the action for output compare
 
-  TIOS|=0x01; // use channel 0 for output compare
-  TSCR1=0x90; // enable timer and allow fast flag clear
-  TCTL2=0x01; // select toggle as the action for output compare
-  TC0=TCNT+45872; // set up TC0 and clear C0F flag
 
- 
-  while(1)
-  {   
-    while(!(TFLG1&0x01)); // Wait till Flag ()TCO == TCNT)
-    TC0=TCNT+45872; // Update TCO to not set off flag until 45872 clocks & clear C0F
-    
-    // If count equals Pause or Duration count based on what the TCTL2 is set to enter if
-    if(count++ == (TCTL2 == 0x01 ? 146 : 21)) { 
-         TCTL2 ^= 0x01; // Toggle the output compare to toggle or of
-         count = 0; // Clear count toastart counting again for the next puase or note duration
-    }
-  }
+     //reg isr
+     *(void (**)()) 0x0fee = tc0_isr;
+     INTR_ON();
+     TIE = 0x01; // locally enable TC0 interrupt
+
+    while(1);
 }
